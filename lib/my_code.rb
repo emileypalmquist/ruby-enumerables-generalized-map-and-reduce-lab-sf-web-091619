965b78ea1c3603e8d0bcb97f679cf5f66ad1c9f5
Emiley Palmquist <emiley.palmquist@icloud.com>
2019-09-04 22:33:58 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,25 @@
 # Your Code Here
+def map(s_array) 
+  new_array = []
+  i = 0 
+ 
+  while i < s_array.length do
+    new_array.push(yield(s_array[i])) 
+    i += 1
+  end
+ 
+  new_array
+end
+
+
+map([1, 2, 3]) {|n| n * -1}
+
+
+def reduce(s_array)
+  i = 0 
+  
+  while i < s_array.length do
+    i += 1
+  end  
+  
+end  
